c68b5abca2bbf7f7e1b5a3aa41657dfe683f7744
BrianOtto <brian.otto@zoho.com>
2023-07-29 01:50:11 -0700

    A lot of refactoring and code cleanup

--- background.js
+++ background.js
@@ -1,16 +1,47 @@
-browser.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request && request.action === 'showChat') {
+var windowChat = null
+
+function toggle(callback, winLeft) {
+    if (windowChat == null) {
         let settings = {
             type: 'popup',
+            
+            // you must add this to your hosts
+            // to debug / run the app locally
+            // 127.0.0.1 discuitchat.local
+            // url: 'http://discuitchat.local',
             url: 'https://discuitchat.net',
-            width: 425,
-            height: 650,
+            
+            // display the window over the sidebar
+            left: winLeft,
+            
+            // expand it until the edge of the screen 
+            // - 20 is the scrollbar width + spacing
+            width: window.screen.width - winLeft - 20,
+            
+            // display the window under the navbar
             top: window.screen.height - 650,
-            left: request.left - 5
+            
+            // expand it until the edge of the screen
+            height: 650
         }
         
         browser.windows.create(settings, function (win) {
-            sendResponse(win)
+            windowChat = win
         })
+    } else {
+        browser.windows.remove(windowChat.id)
+        .then(function() {}, function(err) {
+            windowChat = null
+        })
+    }
+}
+
+browser.runtime.onMessage.addListener(function(request, sender, callback) {
+    if (request) {
+        switch (request.action) {
+            case 'toggle' :
+                toggle(callback, request.left)
+                break
+        }
     }
 })
--- index.js
+++ index.js
@@ -1,38 +1,51 @@
 // about:debugging#/runtime/this-firefox
 
-var settingsWin = 0;
-
-function showChat() {
-    if (settingsWin == 0) {
-        chrome.runtime.sendMessage(
-            {
-                action: 'showChat',
-                left: document.querySelector('.sidebar-right').offsetLeft
-            },
-            function(createdWindow) {
-                chatWin = createdWindow
-            }
-        );
-        
-    } else {
-        browser.windows.remove(chatWin)
-    }
+function toggle() {
+    // send a message to the background 
+    // script to toggle the chat window
+    browser.runtime.sendMessage(
+        {
+            action: 'toggle',
+            // display the window over the right sidebar
+            left: document.querySelector('.sidebar-right').offsetLeft - 5
+        },
+        function(win) {
+            // callback
+        }
+    )
 }
 
+// TODO: get the minimize button to change window height too
+// TODO: fix chat window height / shrinking issue
+// TODO: add a loading screen for the window
 function init() {
-    var navbar = document.querySelector('.navbar .notifications-button')
+    // get the Discuit navbar
+    let dtNavbar = document.querySelector('.navbar')
     
-    if (!navbar) {
+    if (dtNavbar == null) {
+        // retry until React has loaded the navbar
         setTimeout(init, 500)
     } else {
-        var notifyIcon = document.querySelector('.right')
+        // remove the existing chat button
+        let plToggle = document.querySelector('#pl-toggle')
+        if (plToggle) { plToggle.remove() }
         
+        // create a new chat button
         let div = document.createElement('div')
+        div.id = 'pl-toggle'
         div.innerHTML = '💬️'
-        div.onclick = showChat
+        div.onclick = toggle
         div.style.cursor = 'pointer'
         
-        notifyIcon.insertBefore(div, notifyIcon.firstChild)
+        // get the navbar's right column
+        let dtNavbarRight = document.querySelector('.navbar .right')
+        
+        if (dtNavbarRight) {
+            // add the chat button next to the notifications button
+            dtNavbarRight.insertBefore(div, dtNavbarRight.firstChild)
+        } else {
+            console.log('Party Line - The ".navbar .right" column is missing')
+        }
     }
 }
 
